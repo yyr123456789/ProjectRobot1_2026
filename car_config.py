cfd1cfebe6514387238928acf370cb36de3fee4b
yyr123456789 <418955330@qq.com>
2026-06-24 12:46:16 +0800

    Update car_config.py

--- car_config.py
+++ car_config.py
@@ -1,12 +1,17 @@
 from microbit import *
 
-MOTOR_LEFT_FORWARD_PIN = pin0
-MOTOR_LEFT_BACKWARD_PIN = pin1
-MOTOR_RIGHT_FORWARD_PIN = pin2
-MOTOR_RIGHT_BACKWARD_PIN = pin8
+# Left motor pins
+MOTOR_LEFT_FORWARD = pin0
+MOTOR_LEFT_BACKWARD = pin1
 
+# Right motor pins
+MOTOR_RIGHT_FORWARD = pin2
+MOTOR_RIGHT_BACKWARD = pin8
+
+# Speed limits
 MIN_MOTOR_SPEED = 200
 MAX_MOTOR_SPEED = 1023
 DEFAULT_MOTOR_SPEED = 600
 
+# Global storage for encrypted data
 stored_encrypted_face_data = None
