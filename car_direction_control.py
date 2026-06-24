ac9b42f922462a6f360ab71327aa1e5dc7f696d7
yyr123456789 <418955330@qq.com>
2026-06-24 12:48:21 +0800

    Update car_direction_control.py

--- car_direction_control.py
+++ car_direction_control.py
@@ -1,43 +1,30 @@
-from car_config import *
+from car_speed_control import *
 
-def clamp_speed(speed_value: int) -> int:
-    abs_spd = abs(speed_value)
-    return max(MIN_MOTOR_SPEED, min(abs_spd, MAX_MOTOR_SPEED))
-
-def set_left_motor(speed_value: int):
-    spd = clamp_speed(speed_value)
-    if speed_value > 0:
-        MOTOR_LEFT_FORWARD.write_analog(spd)
-        MOTOR_LEFT_BACKWARD.write_analog(0)
-    else:
-        MOTOR_LEFT_FORWARD.write_analog(0)
-        MOTOR_LEFT_BACKWARD.write_analog(spd)
-
-def set_right_motor(speed_value: int):
-    spd = clamp_speed(speed_value)
-    if speed_value > 0:
-        MOTOR_RIGHT_FORWARD.write_analog(spd)
-        MOTOR_RIGHT_BACKWARD.write_analog(0)
-    else:
-        MOTOR_RIGHT_FORWARD.write_analog(0)
-        MOTOR_RIGHT_BACKWARD.write_analog(spd)
-
-def full_stop():
-    set_left_motor(0)
-    set_right_motor(0)
-    display.show(Image.SAD)
-
-def move_forward(speed: int = DEFAULT_MOTOR_SPEED):
-    set_left_motor(speed)
+def pivot_left(speed: int = DEFAULT_MOTOR_SPEED):
+    set_left_motor(-speed // 2)
     set_right_motor(speed)
-    display.show(Image.ARROW_N)
+    display.show(Image.ARROW_W)
 
-def move_backward(speed: int = DEFAULT_MOTOR_SPEED):
-    set_left_motor(-speed)
-    set_right_motor(-speed)
-    display.show(Image.ARROW_S)
+def pivot_right(speed: int = DEFAULT_MOTOR_SPEED):
+    set_left_motor(speed)
+    set_right_motor(-speed // 2)
+    display.show(Image.ARROW_E)
+
+def curve_left(speed: int = DEFAULT_MOTOR_SPEED):
+    set_left_motor(speed // 2)
+    set_right_motor(speed)
+
+def curve_right(speed: int = DEFAULT_MOTOR_SPEED):
+    set_left_motor(speed)
+    set_right_motor(speed // 2)
 
 if __name__ == "__main__":
-    move_forward(800)
-    sleep(2000)
+    move_forward()
+    sleep(1000)
+    pivot_left()
+    sleep(1500)
+    move_forward()
+    sleep(1000)
+    pivot_right()
+    sleep(1500)
     full_stop()
